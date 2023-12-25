a2f868804302d19f214307ff6ac780c997261223
xuanlongdev <zerodev369@gmail.com>
2023-12-25 11:23:13 +0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './Test/test.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
